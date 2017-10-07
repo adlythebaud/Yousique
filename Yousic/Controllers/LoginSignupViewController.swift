57f3ec600e35c3cbbb222f6ba8640a78a174257f
adlythebaud <adlythebaud@gmail.com>
2017-10-07 00:33:15 -0600

    set up registerNewUser method so each user has a separate reference in database

--- Yousic/Controllers/LoginSignupViewController.swift
+++ Yousic/Controllers/LoginSignupViewController.swift
@@ -21,6 +21,7 @@ class LoginSignupViewController: UIViewController {
    @IBOutlet weak var passwordTextField: UITextField!
    @IBOutlet weak var mainActionButton: UIButton!
    @IBOutlet weak var secondaryActionButton: UIButton!
+   @IBOutlet weak var signupErrorLabel: UILabel!
    
    
    //MARK: Actions
@@ -30,33 +31,8 @@ class LoginSignupViewController: UIViewController {
     * DESCRIPTION: authenticate a user into database.
     ***********************************************************/
    @IBAction func mainActionButtonTapped(_ sender: Any) {
-      guard let email = emailTextField.text, let username = usernameTextField.text, let password = passwordTextField.text else {
-         // do some error handling here.
-         print("invalid forms")
-         return
-      }
-      Auth.auth().createUser(withEmail: email, password: password) { (user: User?, error) in
-         // if this if statement is false, then we've successfully authenticated!
-         if error != nil {
-            print(error)
-            return
-         }
-         // make a dictionary of values to send to Firebase
-         let valuesToSendToFirebase = ["Username": username, "Email": email]
-         
-         // update the child values in the node tree in firebase.
-         self.usersRef.updateChildValues(valuesToSendToFirebase, withCompletionBlock: { (err, ref) in
-            if err != nil {
-               print(err)
-               return
-            }
-         })
-         self.performSegue(withIdentifier: "toHomeScreen", sender: nil)
-      }
+      registerNewUser()
    }
-   
-
-   
    
    /**********************************************************
     * NAME: secondaryActionButtonTapped
@@ -79,6 +55,15 @@ class LoginSignupViewController: UIViewController {
       }
    }
    
+   /**********************************************************
+    * NAME: unwindToLogin
+    *
+    * DESCRIPTION: change views when logout button is tapped.
+    ***********************************************************/
+   @IBAction func unwindToLogin(unwindSegue: UIStoryboardSegue) {
+      // should I do any logging out firebase code here?
+   }
+   
    
    //MARK: Methods
    /**********************************************************
@@ -92,22 +77,74 @@ class LoginSignupViewController: UIViewController {
       setupView()
       // instantiate reference to our database.
       ref = Database.database().reference(fromURL: "https://spottystarter.firebaseio.com/")
-      usersRef = ref.child("users")
+      
    }
    
    
    /**********************************************************
-    * NAME: setupView()
+    * NAME: setupView
     *
     * DESCRIPTION: do additional view set up
     ***********************************************************/
    func setupView() {
       mainActionButton.titleLabel?.text = "Sign Up"
       secondaryActionButton.titleLabel?.text = "Login"
+      signupErrorLabel.isHidden = true
    }
    
+   
+   /**********************************************************
+    * NAME: displaySignupError
+    *
+    * DESCRIPTION: If there is an error in signing up, let
+    * user know.
+    ***********************************************************/
+   func displaySignupError(_ error: Error?) {
+      signupErrorLabel.textColor = UIColor.red
+      signupErrorLabel.text = "\(error?.localizedDescription)"
+      signupErrorLabel.isHidden = false
+   }
   
-  
+   /**********************************************************
+    * NAME: registerNewUser
+    *
+    * DESCRIPTION: register a new user in my authorization
+    * and keep their data in the database!
+    ***********************************************************/
+   func registerNewUser() {
+      guard let email = emailTextField.text, let username = usernameTextField.text, let password = passwordTextField.text else {
+         // do some error handling here.
+         print("invalid forms")
+         return
+      }
+      Auth.auth().createUser(withEmail: email, password: password) { (user: User?, error) in
+         // if this if statement is false, then we've successfully authenticated!
+         if error != nil {
+            //            self.displaySignupError(error)
+            print(error)
+            return
+         }
+         // make a dictionary of values to send to Firebase
+         let valuesToSendToFirebase = ["Username": username, "Email": email]
+         
+         guard let uid = user?.uid else {
+            return
+         }
+         // create a reference to the uid child of the users which is child of database
+         self.usersRef = self.ref.child("users").child(uid)
+         
+         // update the "users" part of the database.
+         self.usersRef.updateChildValues(valuesToSendToFirebase, withCompletionBlock: { (err, ref) in
+            if err != nil {
+               print(err)
+               return
+            }
+         })
+         
+         // once done, perform segue.
+         self.performSegue(withIdentifier: "toHomeScreen", sender: nil)
+      }
+   }
 
 
 }
